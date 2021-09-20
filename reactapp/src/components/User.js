7033185963064e996ad24f6181a57cd7390d489c
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-20 02:43:19 +0200

    updated

--- reactapp/src/components/User.js
+++ reactapp/src/components/User.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 class User extends Component {
+    static defaultProps = {
+        name : 'Default Name',
+        salary : 'Default Salary',
+        department : 'Default Department'
+    }
     render() {
+        const {name,salary,department}=this.props;
         return (
-            <div>
-                <form >
-                    <input type="text" />
-                    <button>Send</button>
-                </form>
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
+                    </div>
+                    <div className="card-body">
+                        <p className="card-text">Salary : {salary}</p>
+                        <p className="card-text">Department : {department}</p>
+                    </div>
+                </div>
+                
             </div>
         )
     }
+}
+
+User.propTypes={
+    name : PropTypes.string.isRequired,
+    salary : PropTypes.string.isRequired,
+    department : PropTypes.string.isRequired
 }
 export default User;
