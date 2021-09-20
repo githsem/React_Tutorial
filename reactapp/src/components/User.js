b697ba08bebf24895e5378cbd262f7f0e3087165
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-20 19:23:24 +0200

    updated

--- reactapp/src/components/User.js
+++ reactapp/src/components/User.js
@@ -3,24 +3,38 @@ import PropTypes from 'prop-types'
 
 
 class User extends Component {
+    state = {
+        isVisible : false
+    }
     static defaultProps = {
         name : 'Default Name',
         salary : 'Default Salary',
         department : 'Default Department'
     }
+    onClickEvent = (e) => {
+        console.log(this);
+    }
     render() {
+
+        //Destructing
         const {name,salary,department}=this.props;
+        const{isVisible} = this.state;
         return (
             <div className="col-md-8 mb-4">
                 <div className="card">
                     <div className="card-header d-flex justify-content-between">
-                        <h4 className="d-inline">{name}</h4>
+                        <h4 className="d-inline" onClick={this.onClickEvent} >{name}</h4>
                         <i className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text">Salary : {salary}</p>
-                        <p className="card-text">Department : {department}</p>
-                    </div>
+                    {
+                    isVisible ? 
+                        <div className="card-body">
+                            <p className="card-text">Salary : {salary}</p>
+                            <p className="card-text">Department : {department}</p>
+                        </div>
+                        : null
+                    }
+                    
                 </div>
                 
             </div>
