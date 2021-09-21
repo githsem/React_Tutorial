681317796efede18191c7d985d0e17ffde8504bc
Hasan Huseyin Semiz <hhsem49@gmail.com>
2021-09-21 15:32:42 +0200

    updated

--- reactapp/src/components/User.js
+++ reactapp/src/components/User.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import UserConsumer from '../context'
 
 class User extends Component {
     state = {
@@ -16,35 +16,48 @@ class User extends Component {
             isVisible : ! this.state.isVisible
         })
     }
-    onDeleteUser = (e) => {
-        //const {id} = this.props;
+    onDeleteUser = (dispatch, e) => {
+        const {id} = this.props;
         // Consumer Dispatch
+        dispatch({type : "DELETE_USER", payload:id});
     }
     render() {
 
         //Destructing
         const {name,salary,department}=this.props;
         const{isVisible} = this.state;
-        return (
-            <div className="col-md-8 mb-4">
-                <div className="card">
-                    <div className="card-header d-flex justify-content-between">
-                        <h4 className="d-inline" onClick={this.onClickEvent} >{name}</h4>
-                        <i className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
-                    </div>
-                    {
-                    isVisible ? 
-                        <div className="card-body">
-                            <p className="card-text">Salary : {salary}</p>
-                            <p className="card-text">Department : {department}</p>
+
+        return(
+            <UserConsumer>
+                {
+                    value =>{
+                        const{dispatch} = value;
+                        return (
+                        <div className="col-md-8 mb-4">
+                            <div className="card">
+                                <div className="card-header d-flex justify-content-between">
+                                    <h4 className="d-inline" onClick={this.onClickEvent} >{name}</h4>
+                                    <i onClick={this.onDeleteUser.bind(this,dispatch)} className="far fa-trash-alt" style={{cursor : "pointer"}}></i>
+                                </div>
+                                {
+                                isVisible ? 
+                                    <div className="card-body">
+                                        <p className="card-text">Salary : {salary}</p>
+                                        <p className="card-text">Department : {department}</p>
+                                    </div>
+                                    : null
+                                }
+                                
+                            </div>
+                            
                         </div>
-                        : null
+                    )
                     }
-                    
-                </div>
-                
-            </div>
+                }
+            </UserConsumer>
         )
+
+        
     }
 }
 
